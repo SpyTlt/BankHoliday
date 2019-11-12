110c5d6f22f347d41c9d9d1f5097b6481e1ae331
Vitautas <Vitautas@LINA>
2019-11-12 01:46:15 -0500

    Add project files.

---
+++ BankHoliday/BankHoliday.cs
@@ -0,0 +1,310 @@
+﻿// (c) Vitas Ramanchauskas www.SpyTlt.com, MIT license
+// BankHolidays for US markets. 
+// Key methods:
+// nowEst - current DateTime in EST timezone
+// isWorkingday - checks if the market is open on a given date
+// isMarketOpenNow, isMarketOpenAt - check if the market is open right now or at specified DateTime
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace BankHolidayNS
+{
+    /// <summary>
+    /// Support for US bank holidays.
+    /// </summary>
+    public class BankHoliday
+    {
+        public enum MarketTime { beforeOpen, open, afterClose }
+
+
+        public static TimeZoneInfo estTimeZone { get; private set; } = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+
+        /// <summary>
+        /// Convert local DateTime to EST timezone
+        /// </summary>
+        /// <param name="dt">DateTime to convert</param>
+        /// <returns>Given DateTime in EST timezone</returns>
+        static public DateTime toEst(DateTime dt) =>
+            TimeZoneInfo.ConvertTime(dt, estTimeZone);
+
+        static public DateTime fromEst(DateTime dt) =>
+            TimeZoneInfo.ConvertTime(dt, estTimeZone, TimeZoneInfo.Local);
+
+        /// <summary>
+        /// Current DateTime in EST timezone
+        /// </summary>
+        static public DateTime nowEST => toEst(DateTime.Now);
+
+
+        /// <summary>
+        /// List of extra days when market was closed (9/11 for example)
+        /// </summary>
+        public static readonly HashSet<DateTime> extraCloseDates = new HashSet<DateTime> { new DateTime(2001, 9, 11), new DateTime(2001, 9, 12),
+             new DateTime(2001, 9, 13), new DateTime(2001, 9, 14),  new DateTime(2004, 6, 11), new DateTime(2007, 1, 2),
+             new DateTime(2012, 10, 29), new DateTime(2012, 10, 30), new DateTime(2018, 12, 5) };
+
+        /// <summary>
+        /// When is Good Friday
+        /// </summary>
+        /// <param name="year">Given year</param>
+        /// <returns>Exact date</returns>
+        public static DateTime getGoodFriday(int year)
+        {
+            int day = 0;
+            int month = 0;
+
+            int g = year % 19;
+            int c = year / 100;
+            int h = (c - (int)(c / 4) - (int)((8 * c + 13) / 25) + 19 * g + 15) % 30;
+            int i = h - (int)(h / 28) * (1 - (int)(h / 28) * (int)(29 / (h + 1)) * (int)((21 - g) / 11));
+
+            day = i - ((year + (int)(year / 4) + i + 2 - c + (int)(c / 4)) % 7) + 28;
+            month = 3;
+
+            if (day > 31)
+            {
+                month++;
+                day -= 31;
+            }
+
+            return new DateTime(year, month, day).AddDays(-2);
+        }
+
+        /// <summary>
+        /// Third Friday in the month
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <returns></returns>
+        private static DateTime thirdMonday(int year, int month)
+        {
+            var d = new DateTime(year, month, 1);
+            return new DateTime(year, month, 21 - (((int)d.DayOfWeek + 5) % 7));
+        }
+
+        private static DateTime fourthThursday(int year, int month)
+        {
+            var d = new DateTime(year, month, 1);
+            return new DateTime(year, month, 28 - (((int)d.DayOfWeek + 2) % 7));
+        }
+
+        public static readonly TimeSpan normalCloseTime = new TimeSpan(16, 0, 0);
+        public static readonly TimeSpan earlyCloseTime = new TimeSpan(13, 0, 0);
+
+/// <summary>
+/// When does the market close on a specified date
+/// </summary>
+/// <param name="_date">Date to check</param>
+/// <returns></returns>
+        public static TimeSpan getCloseTimeForDate(DateTime _date)
+        {
+            var date = _date.Date;
+            switch (date.Month)
+            {
+                case 7:
+                    if (date.Day == 3 && !isWeekend(date.AddDays(1)))
+                        return earlyCloseTime;
+                    else
+                        return normalCloseTime;
+                case 11:
+                    var thanksGivenAfter = fourthThursday(date.Year, 11).AddDays(1);
+                    return (date == thanksGivenAfter)? earlyCloseTime : normalCloseTime;
+
+                case 12:
+                    if (date.Day == 24 && !isWeekend(date.AddDays(1)))
+                        return earlyCloseTime;
+                    else
+                        return normalCloseTime;
+                default:
+                    return normalCloseTime;
+            }
+        }
+
+        public static bool isEarlyCloseDay(DateTime date) =>
+            getCloseTimeForDate(date.Date).TotalHours < normalCloseTime.TotalHours;
+
+        /// <summary>
+        /// Check if the given date is holiday. Returns false for weekends.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static bool isHoliday(DateTime date) => getHolidays(date.Year).Contains(date.Date);
+
+        public static bool isHolidayToday => isHoliday(nowEST);
+
+        public static bool isExtraClosedDate(DateTime date) => extraCloseDates.Contains(date.Date);
+
+        public static bool isWeekend(DateTime date) => 
+            date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+
+        public static bool isWorkingDay(DateTime date)
+        {
+            if (isWeekend(date))
+                return false;
+            return !isHoliday(date) && !isExtraClosedDate(date);
+        }
+
+        public static List<DateTime> getHolidays(int yearStart, int yearCount)
+        {
+            var list = new List<DateTime>();
+            for (int i = 0; i < yearCount; i++)
+                list.AddRange(getHolidays(yearStart + i));
+            return list;
+        }
+
+
+        public static DateTime? getNewYear(int year)
+        {
+            var newYearsDate = new DateTime(year, 1, 1);
+            switch (newYearsDate.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return newYearsDate.AddDays(1);
+
+                case DayOfWeek.Saturday:
+                    return null;
+
+                default:
+                    return newYearsDate;
+            }
+
+        }
+
+        public static DateTime getMartinLutherDay(int year) => thirdMonday(year, 1);
+        public static DateTime getPresidentDay(int year) => thirdMonday(year, 2);
+        public static DateTime getThanksGiving(int year) => fourthThursday(year, 11);
+
+        public static DateTime getIndependenceDay(int year) => adjustForWeekendHoliday(new DateTime(year, 7, 4));
+        public static DateTime getChristmasDay(int year) => adjustForWeekendHoliday(new DateTime(year, 12, 25));
+
+        public static DateTime getMemorialDay(int year)
+        {
+            var memorialDay = new DateTime(year, 5, 31);
+            var dayOfWeek = memorialDay.DayOfWeek;
+            while (dayOfWeek != DayOfWeek.Monday)
+            {
+                memorialDay = memorialDay.AddDays(-1);
+                dayOfWeek = memorialDay.DayOfWeek;
+            }
+            return memorialDay;
+        }
+
+
+        public static DateTime getLaborDay(int year)
+        {
+            var laborDay = new DateTime(year, 9, 1);
+            var dayOfWeek = laborDay.DayOfWeek;
+            while (dayOfWeek != DayOfWeek.Monday)
+            {
+                laborDay = laborDay.AddDays(1);
+                dayOfWeek = laborDay.DayOfWeek;
+            }
+            return laborDay;
+        }
+
+
+        static ConcurrentDictionary<int, HashSet<DateTime>> dicHolidaysByYear = new ConcurrentDictionary<int, HashSet<DateTime>>();
+
+        public static HashSet<DateTime> getHolidays(int year)
+        {
+            if (dicHolidaysByYear.ContainsKey(year))
+                return dicHolidaysByYear[year];
+
+            var holidays = new HashSet<DateTime>();
+
+            var newYearsDate = getNewYear(year);
+            if(newYearsDate!=null)
+                holidays.Add((DateTime)newYearsDate);
+
+            holidays.Add(getMartinLutherDay(year));
+            holidays.Add(getPresidentDay(year));
+            holidays.Add(getGoodFriday(year));
+            holidays.Add(getMemorialDay(year));
+            holidays.Add(getIndependenceDay(year));
+            holidays.Add(getLaborDay(year));
+            holidays.Add(getThanksGiving(year));
+            holidays.Add(getChristmasDay(year));
+
+            dicHolidaysByYear[year] = holidays;
+
+            return holidays;
+        }
+
+        private static DateTime adjustForWeekendHoliday(DateTime holiday)
+        {
+            if (holiday.DayOfWeek == DayOfWeek.Saturday)
+                return holiday.AddDays(-1);
+            return holiday.DayOfWeek == DayOfWeek.Sunday ? holiday.AddDays(1) : holiday;
+        }
+
+        public static DateTime nextTradingDayAfter(DateTime afterDt)
+        {
+            var dt = afterDt.Date;
+            do
+                dt = dt.AddDays(1);
+            while (!isWorkingDay(dt));
+            return dt;
+        }
+
+ /// <summary>
+ /// get the last trading day before a given date
+ /// </summary>
+ /// <param name="beforeDt"></param> date to check
+ /// <returns>last trading day before a given date</returns>
+        public static DateTime prevTradingDayBefore(DateTime beforeDt)
+        {
+            var dt = beforeDt.Date;
+            do
+                dt = dt.AddDays(-1);
+            while (!isWorkingDay(dt));
+            return dt;
+        }
+
+        public static DateTime nextTradingDay => nextTradingDayAfter(nowEST);
+        public static DateTime prevTradingDay => prevTradingDayBefore(nowEST);
+
+        public static bool isEarlyCloseToday => isEarlyCloseDay(nowEST);
+
+        public static bool isWorkingDayToday => isWorkingDay(nowEST);
+
+        public static TimeSpan closeTimeForToday => getCloseTimeForDate(nowEST);
+
+        public static MarketTime checkTimeNow => checkTime(nowEST);
+
+        public static MarketTime checkTime(DateTime estTime)
+        {
+            var m = estTime.TimeOfDay.TotalMinutes;
+            if (m < 9 * 60 + 30)
+                return MarketTime.beforeOpen;
+            if (m >= getCloseTimeForDate(estTime).TotalMinutes)
+                return MarketTime.afterClose;
+            return MarketTime.open;
+        }
+
+        static public TimeSpan timeLeftToOpen => TimeSpan.FromMinutes(9 * 60 + 30) - nowEST.TimeOfDay;
+        static public TimeSpan timeLeftToClose => closeTimeForToday - nowEST.TimeOfDay;
+
+
+        /// <summary>
+        /// Is market open at given date and time, weekends, holidays, eearly hours, everything is counted.
+        /// </summary>
+        /// <param name="estTime">Date and time to check</param>
+        /// <returns></returns>
+        public static bool isMarketOpenAt(DateTime estTime)
+        {
+            if (!isWorkingDay(estTime))
+                return false;
+            return checkTime(estTime) == MarketTime.open;
+        }
+
+        public static bool isMarketOpenNow => isMarketOpenAt(nowEST);
+
+
+        public static bool isDstChangeInBetween(DateTime dt1, DateTime dt2) =>
+            estTimeZone.IsDaylightSavingTime(dt1) != estTimeZone.IsDaylightSavingTime(dt2);
+
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankHolidayNS;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void checkDstDetect()
+        {
+            Assert.IsTrue(BankHoliday.isDstChangeInBetween(new DateTime(2016, 3, 12), new DateTime(2016, 3, 14)));
+            Assert.IsTrue(BankHoliday.isDstChangeInBetween(new DateTime(2016, 11, 6), new DateTime(2016, 11, 7)));
+            Assert.IsFalse(BankHoliday.isDstChangeInBetween(new DateTime(2016, 11, 5), new DateTime(2016, 11, 6)));
+            Assert.IsFalse(BankHoliday.isDstChangeInBetween(new DateTime(2016, 3, 12), new DateTime(2016, 3, 13)));
+        }
+
+        [TestMethod]
+        public void testCloseTime()
+        {
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2015, 11, 27)).TotalHours);
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2015, 12, 24)).TotalHours);
+            Assert.AreEqual(16, BankHoliday.getCloseTimeForDate(new DateTime(2016, 12, 24)).TotalHours);
+            Assert.AreEqual(16, BankHoliday.getCloseTimeForDate(new DateTime(2016, 7, 2)).TotalHours);
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2014, 7, 3)).TotalHours);
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2014, 12, 24)).TotalHours);
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2014, 11, 28)).TotalHours);
+
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2013, 7, 3)).TotalHours);
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2013, 11, 29)).TotalHours);
+            Assert.AreEqual(13, BankHoliday.getCloseTimeForDate(new DateTime(2013, 12, 24)).TotalHours);
+        }
+
+        [TestMethod]
+        public void testHolidays()
+        {
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 1, 1)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 1, 21)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 2, 18)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 4, 19)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 5, 27)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 7, 4)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 9, 2)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 11, 28)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 12, 25)));
+
+
+            Assert.IsTrue(BankHoliday.isWorkingDay(new DateTime(2019, 7, 3)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2020, 7, 3)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2021, 7, 5)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2021, 12, 24)));
+
+            Assert.IsTrue(BankHoliday.isWorkingDay(new DateTime(2019, 6, 21)));
+            Assert.IsFalse(BankHoliday.isWorkingDay(new DateTime(2019, 6, 22)));
+        }
+
+    }
+}
